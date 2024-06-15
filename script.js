4b1ca66d3fc872e29d602db4852f1817d91f9cce
dyeiksonmauricio <102035048+dyeiksonmauricio@users.noreply.github.com>
2024-06-15 20:19:28 +0000

    alteraçoes no projeto

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const caixaResultado = document.querySelector(‘.caixa - resultado’);
 const textoResultado = document.querySelector(‘.texto - resultado’);
 const perguntas = [
     {
-        enunciado: “Pergunta 1”,
-    alternativas: [“Alternativa 1”, “Alternativa 2”,"Alternativa 3"],
+     enunciado: “Pergunta 1”,
+     alternativas: [“Alternativa 1”, “Alternativa 2”,"Alternativa 3"],
     },
 {
     enunciado: “Pergunta 2”,
